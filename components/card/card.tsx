086a17c265192c86ec580e87a851d6aab5fb0dc2
ozulett <oliverzulett@gmail.com>
2022-03-24 23:27:52 -0400

    update navbar

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -11,41 +11,23 @@ export interface CardProps {
 }
 
 function Card(props: CardProps) {
-  const {
-    id,
-    name,
-    image,
-    description,
-    rating
-  } = props;
-  const IMAGE_API = 'https://cdn.pixabay.com/';
+  const { id, name, image, description, rating } = props;
+  const IMAGE_API = "https://cdn.pixabay.com/";
   const imagePath = image.slice(IMAGE_API.length);
-  
+
   return (
-    <div className="card w-full bg-base-100 shadow-xl">
+    <div className="card w-96 bg-base-100 shadow-xl image-full">
       <figure>
-        <Image
-          src={imagePath}
+        <img
+          src="https://api.lorem.space/image/shoes?w=400&h=225"
           alt="Shoes"
-          className="max-w-sm rounded-lg shadow-2xl"
-          width={500}
-          height={325}
         />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">{name}</h2>
-        <p>{description}</p>
-        <div className="rating gap-1">
-          <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" />
-          <input type="radio" name="rating-3" className="mask mask-heart bg-orange-400" checked />
-          <input type="radio" name="rating-3" className="mask mask-heart bg-yellow-400" />
-          <input type="radio" name="rating-3" className="mask mask-heart bg-lime-400" />
-          <input type="radio" name="rating-3" className="mask mask-heart bg-green-400" />
-        </div>
-        <div className="card-actions justify-end pt-3">
-          <Link href={`/movies/${id}`}>
-            <button className="btn btn-outline btn-accent btn-sm">Ver Mas</button>
-          </Link>
+        <h2 className="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Buy Now</button>
         </div>
       </div>
     </div>
@@ -53,3 +35,33 @@ function Card(props: CardProps) {
 }
 
 export default Card;
+
+{
+  /* <div className="card w-full bg-base-100 shadow-xl">
+<figure>
+  <Image
+    src={imagePath}
+    alt="Shoes"
+    className="max-w-sm rounded-lg shadow-2xl"
+    width={500}
+    height={325}
+  />
+</figure>
+<div className="card-body">
+  <h2 className="card-title">{name}</h2>
+  <p>{description}</p>
+  <div className="rating gap-1">
+    <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" />
+    <input type="radio" name="rating-3" className="mask mask-heart bg-orange-400" checked />
+    <input type="radio" name="rating-3" className="mask mask-heart bg-yellow-400" />
+    <input type="radio" name="rating-3" className="mask mask-heart bg-lime-400" />
+    <input type="radio" name="rating-3" className="mask mask-heart bg-green-400" />
+  </div>
+  <div className="card-actions justify-end pt-3">
+    <Link href={`/movies/${id}`}>
+      <button className="btn btn-outline btn-accent btn-sm">Ver Mas</button>
+    </Link>
+  </div>
+</div>
+</div> */
+}
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -4,45 +4,95 @@ import Link from "next/link";
 function Navbar() {
   return (
     <div className="navbar bg-base-100">
-      <div className="flex-1">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <Link href={`/`}>
+                <a>Series</a>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/`}>
+                <a>Peliculas</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="navbar-center">
         <Link href={`/`}>
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+          <a className="btn btn-ghost normal-case text-xl">
+            De NETFLIX lo mejor
+          </a>
         </Link>
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal p-0">
-          <li>
-            <Link href={`/`}>
-              <a>Item 1</a>
-            </Link>
-          </li>
-          <li tabIndex={0}>
-            <a>
-              Parent
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="p-2 bg-base-100">
-              <li>
-                <Link href={`/`}>
-                  <a>Submenu 1</a>
-                </Link>
-              </li>
-              <li>
-                <Link href={`/`}>
-                  <a>Submenu 2</a>
-                </Link>
-              </li>
-            </ul>
-          </li>
-        </ul>
+      <div className="navbar-end">
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered"
+          ></input>
+        </div>
+        <div className="dropdown dropdown-end px-5">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src="https://api.lorem.space/image/face?hash=33791" />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <div className="dropdown dropdown-left">
+                <label >
+                  Crear Reseña
+                </label>
+                <ul
+                  
+                  className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+                >
+                  <li>
+                    <Link href={`/`}>
+                      <a>Pelicula</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href={`/`}>
+                      <a>Serie</a>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li>
+              <Link href={`/`}>
+                <a>Perfil</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
